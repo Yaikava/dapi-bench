8ba52f778fab5c31aa8508d226e232ddec7b3750
Yaikawa <83710104+Yaikava@users.noreply.github.com>
2021-06-04 18:27:11 +0200

    👌 IMPROVE: linux script

--- start-linux.sh
+++ start-linux.sh
@@ -1,1 +1,6 @@
-#TODO
+#!/bin/bash
+
+declare -a StringArray=("discordeno" "harmony" "discordjs" "eris" "detritus")
+for lib in ${StringArray[@]}; do
+    x-terminal-emulator -e --hold "npm run ${lib}"
+done
